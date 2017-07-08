18002cc25a51e33542ff750b132f16d19796361f
tanyaa-singh <tanyasingh1996@gmail.com>
2017-07-08 14:52:54 +0530

    Modified Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'herokuapp', port: 3000 }
+   config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'railsproject100@gmail.com',
+    password:             'railsproject',
+    authentication:       :login,
+    enable_starttls_auto: true
+  }
 end
